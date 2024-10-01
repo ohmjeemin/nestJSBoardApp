e4f081526ba6b8ba81989a118041a9eff950cd31
ohmjeemin <ohmjeemin@gmail.com>
2024-10-02 00:42:58 +0900

    updateBoard서비스, 컨트롤러 작성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Patch, Post} from '@nestjs/common';
 import {BoardsService} from './boards.service';
-import {Board} from './boards.model';
+import {Board, BoardStatus} from './boards.model';
 import {CreateBoardDto} from './dto/create-board.dto';
 
 @Controller('boards')
@@ -20,5 +20,18 @@ export class BoardsController {
   @Get('/:id')
   getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Get('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    this.boardsService.deleteBoardById(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): Board {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
